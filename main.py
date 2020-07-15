5e780d0162399ebe31cbf9453aef2f608202e1d4
linus Granath <linusgh@axis.com>
2020-07-15 22:35:46 +0200

    Change names

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from Entities.Course import Course
-from Entities.Player import Player
-course_selection = input("write your course name")
-par = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
-course = Course(course_selection,par)
+from entities.Course import Course
+from entities.Player import Player
+
+course_name = input("write your course name")
+par = [3,3,4,3,3,4,3,3,4,3,4,3,3,4,3,4,3,4]
+course = Course(course_name,par)
+
 print(course.name)
 print(course.par)
 print(course.total_tracks())
