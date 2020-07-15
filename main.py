f1a3bf39b61bebaca0e135176dd426d5cfe4d6ce
linus Granath <linusgh@axis.com>
2020-07-15 22:27:06 +0200

    Added player to main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Entities.Course import Course
+from Entities.Player import Player
 course_selection = input("write your course name")
 par = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
 course = Course(course_selection,par)
@@ -6,24 +7,5 @@ print(course.name)
 print(course.par)
 print(course.total_tracks())
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+player = Player("Linus")
+print(player.name)
