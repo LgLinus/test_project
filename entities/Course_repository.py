43da6edc0d75dcb5bee832876dba26f1ac6b4eb1
fredrik <klosansa1@hotmail.com>
2020-07-17 19:43:04 +0200

    courserepository update

--- entities/Course_repository.py
+++ entities/Course_repository.py
@@ -1,5 +1,28 @@
+import entities.Course
+class courses:
+    def get_courses ():
+        bulltofta = entities.Course.Course("bulltofta:",[3,3,3,3,3,3,4,3,3,4,3,3,3,4,3,4,3,3])
+        sibbarp = entities.Course.Course("sibbarp:",[4,5,6])
 
-bulltofta = [3, 3, 3, 3, 3, 3, 4, 3, 3, 4, 3, 3, 3, 4, 3, 4, 3, 3]
+        return bulltofta,sibbarp
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
