6ace5dd0de5fec640b5bb3b59eea9692305bacc0
SweSebbe91 <bassepersson@hotmail.com>
2020-07-26 19:25:55 +0200

    uppdaterat Sibbarps banor

--- dao/CourseRepository.py
+++ dao/CourseRepository.py
@@ -1,8 +1,8 @@
 import entities.Course
 class course_repository:
     def get_courses ():
-        bulltofta = entities.Course.Course("bulltofta:",[3,3,3,3,3,3,4,3,3,4,3,3,3,4,3,4,3,3])
-        sibbarp = entities.Course.Course("sibbarp:",[4,5,6])
+        bulltofta = entities.Course.Course("bulltofta: ",[3,3,3,3,3,3,4,3,3,4,3,3,3,4,3,4,3,3])
+        sibbarp = entities.Course.Course("sibbarp: ",[3, 3, 3, 3, 3, 3, 3, 4, 3])
 
 
         return [bulltofta,sibbarp]
